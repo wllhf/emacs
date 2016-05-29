3a528c53a9a57f5ab3c701aade82e284e17d4a70
M. Woellhaf <manuelwoellhaf@gmail.com>
2016-05-29 11:45:45 +0200

    Made IPython the default interpreter.

---
+++ config/python-settings.el
@@ -0,0 +1,12 @@
+;; Python settings
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'python)
+
+;; Use IPython as default interpreter
+(setq python-shell-interpreter "ipython")
+
+;; Enable Elpy Python IDE features
+(elpy-enable)
+
+(provide 'python-settings)
+  
--- init.el
+++ init.el
@@ -1,14 +1,15 @@
 (add-to-list 'load-path "~/.emacs.d/config")
+;; general
 (require 'package-management)
 (require 'general-settings)
-;; (require 'helm-settings)
+;; packages
 (require 'ido-settings)
 (require 'magit-settings)
-(require 'latex-settings)
 (require 'org-settings)
-
-;; require and enable packages
-(elpy-enable)
+;; languages
+(require 'latex-settings)
+(require 'python-settings)
+;; load theme and maximize window
 (load-theme 'zenburn t)
 (toggle-frame-maximized)
 
