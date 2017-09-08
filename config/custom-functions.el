6bfb2e6e1974e0d8f882b1632f24cf3426ef0f86
mw <manuelwoellhaf@gmail.com>
2017-09-08 19:29:27 +0200

    Deleted obsolete file config/custom-functions.el.

--- config/custom-functions.el
+++
@@ -1,6 +0,0 @@
-;; Custom functions
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; function to check if all required packages (the ones in required-packages variable) are installed
-(defun packages-installed-p ()
-  (cl-every 'package-installed-p required-packages))
